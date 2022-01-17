21d45e8492786d24a23d3a20faed821ae0feb27b
fearzyn <ramirez.fernando2003@gmail.com>
2022-01-16 19:03:37 -0600

    works

--- agent.py
+++ agent.py
@@ -4,6 +4,8 @@ import random
 import numpy as np
 from collections import deque
 from game import SnakeGameAI, Direction, Point
+from model import Linear_QNet, QTrainer
+from helper import plot
 
 
 MAX_MEMORY = 100_000
@@ -14,10 +16,10 @@ class Agent:
     def __init__(self):
         self.n_games = 0
         self.epsilon = 0 # randomness
-        self.gamma = 0 # discount rate
+        self.gamma = 0.9 # discount rate (smaller then 1)
         self.memory = deque(maxlen=MAX_MEMORY) # popleft()
-        self.model = None # Todo
-        self.trainer = None # To do
+        self.model = Linear_QNet(11, 256, 3) # neural network layers
+        self.trainer = QTrainer(self.model, lr=LR, gamma=self.gamma)
         # TODO: model, trainer
 
     def get_state(self, game):
@@ -91,7 +93,7 @@ class Agent:
             final_move[move] = 1
         else:
             state0 = torch.tensor(state, dtype=torch.float)
-            prediction = self.model.predict(state0)
+            prediction = self.model(state0)
             move = torch.argmax(prediction).item()
             final_move[move] = 1
         return final_move
@@ -129,9 +131,14 @@ def train():
 
             if score > record:
                 record = score
-                # agent.model.save()
+                agent.model.save()
             print('Game', agent.n_games, 'Score', score, 'Record', record)
-            # TODO: plot
+            
+            plot_scores.append(score)
+            total_score += score
+            mean_score = total_score / agent.n_games
+            plot_mean_scores.append(mean_score)
+            plot(plot_scores, plot_mean_scores)
 
 
 
